7bc0a922c2843694b97a3bc59249f5d2f8ec55f2
johnrhampton <johnrhampton@gmail.com>
2016-05-12 20:44:42 -0400

    adding User create and login routes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * UserController
  *
@@ -6,6 +8,35 @@
  */
 
 module.exports = {
-	
+  /**
+   * POST /user
+   */
+  create: create,
+
+  /**
+   * POST /user/login
+   */
+  login: login
 };
 
+function create(req, res) {
+  UserService.register(req.body)
+    .then((profile) => {
+      return res.json(profile);
+    })
+    .catch((err) => {
+      err.message = err.message || 'Unable to register';
+      return res.badRequest(err);
+    });
+}
+
+function login(req, res) {
+  UserService.authenticate(req.body)
+    .then((profile) => {
+      return res.json(profile);
+    })
+    .catch((err) => {
+      err.message = err.message || 'Could not login, have you registered?';
+      return res.badRequest(err);
+    });
+}
