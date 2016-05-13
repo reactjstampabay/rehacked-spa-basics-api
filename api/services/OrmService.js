d8ef1f9dc107cc1385f98588c2015be202eaf6fc
johnrhampton <johnrhampton@gmail.com>
2016-05-12 21:09:32 -0400

    adding ORMService for common operations

---
+++ api/services/OrmService.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  expectModel: expectModel,
+  expectNoModel: expectNoModel
+};
+
+function expectModel(model) {
+  return new Promise(function(resolve, reject) {
+    return model ? resolve(model) : reject(new Error());
+  });
+}
+
+function expectNoModel(model) {
+  return new Promise(function(resolve, reject) {
+    return model ? reject(new Error()) : resolve(model);
+  });
+}
